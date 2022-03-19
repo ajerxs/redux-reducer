b2ff68764955ecb60b8349619619957a95fef662
Andrew Eames <ajerxs@gmail.com>
2022-03-19 18:31:08 -0400

    commit tree d431aec509c27822e6539253bb9afa302cd1fff8

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+    switch (action.type) {
+        case "ADD_FRIEND":
+            state.friends.push(action.friend)
+            return {
+                friends: state.friends
+            }
+        case "REMOVE_FRIEND":
+            const removeFriend = state.friends.filter(function(value, index, arr) {
+                return value.id !== action.id
+            }) 
+            return {
+                friends: removeFriend
+            }
+        default:
+            return state
+    }
 }
